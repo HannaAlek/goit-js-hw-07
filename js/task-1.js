89421b4e55cd4229870e09e1429080b4f11b6818
HannaAlek <aniuta_alex@ukr.net>
2024-01-20 21:50:51 +0200

    task-1

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+const categories=document.querySelector("#categories");
+const categoriesItems=categories.children.length;
+console.log(`Number of categories: ${categoriesItems}`);
+    const categoryName=document.querySelector("h2").textContent; 
+    const categoryElements=document.querySelector("li").length; 
+    
+
+
+console.log(`Category: ${categoryName}`);
+console.log(`Elements: ${categoryElements}`);
+
