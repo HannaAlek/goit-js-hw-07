d532ca30bef5c02846ec41146f35d448148f924b
HannaAlek <aniuta_alex@ukr.net>
2024-03-11 20:29:14 +0200

    task 6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,32 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnCreate=document.querySelector('button[data-create]');
+const btnDestroy=document.querySelector('button[data-destroy]');
+const boxes=document.querySelector('#boxes');
+const input=document.querySelector('input');
+
+function createBoxes(){
+  const amount=Number(input.value);
+  if(amount<1 || amount>100){return}
+
+const square=document.createDocumentFragment();
+for (let i=0; i<amount; i++){
+  let size=30+i*10;
+  const box=document.createElement('div');
+  box.style.width=`${size}px`;
+  box.style.height=`${size}px`;
+  box.style.backgroundColor=getRandomHexColor();
+  square.append(box);
+}
+boxes.innerHTML="";
+boxes.append(square);
+input.value="";
+}
+function destroyBoxes(event){
+  boxes.innerHTML=""
+}
+
+btnCreate.addEventListener('click', createBoxes)
+btnDestroy.addEventListener('click', destroyBoxes)
